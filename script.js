ca5ff7fc1e56a27796628ba64e6f71c562d76517
Bidooflolz <jakub.swiderski19@gmail.com>
2021-02-27 20:47:37 +0100

    pierwszy commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider' ).mount();
+} );
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider', {
+		'cover'      : true,
+		'heightRatio': 0.8,
+	} ).mount();
+} );
